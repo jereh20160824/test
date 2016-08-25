e100c912d77c83318f2c67d791ad0de219a9fe19
jerehedu <jerehedu@luoyurong.local>
2016-08-25 11:05:35 +0800

    源文件

---
+++ 825LuoTest/825LuoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  825LuoTest
+//
+//  Created by jerehedu on 16/8/25.
+//  Copyright © 2016年 jerehedu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 825LuoTest/825LuoTest/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  825LuoTest
+//
+//  Created by jerehedu on 16/8/25.
+//  Copyright © 2016年 jerehedu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@">>>>>>>>>>>>>>>> ^_^ <<<<<<<<<<<<<<<<<<<");
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
